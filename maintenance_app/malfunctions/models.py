79b6cd5e319c053aa99f8ff4c45292143b346b5b
d-gorecki <ja.dawidgorecki@gmail.com>
2022-09-26 13:18:11 +0200

    feat: malfunctions pending & service report models implemented

--- maintenance_app/malfunctions/models.py
+++ maintenance_app/malfunctions/models.py
@@ -9,3 +9,23 @@ class MalfunctionReport(models.Model):
     datetime = models.DateTimeField(auto_now_add=True)
     description = models.TextField()
     image = models.ImageField(upload_to="media/malfunction_issues")
+
+
+class MalfunctionPending(models.Model):
+    machine = models.ForeignKey(Machine, on_delete=models.CASCADE)
+    malfunction_report = models.OneToOneField(
+        MalfunctionReport, on_delete=models.CASCADE
+    )
+
+    # prop unnecessary model
+
+
+class ServiceReport(models.Model):
+    machine = models.ForeignKey(Machine, on_delete=models.SET_DEFAULT, default=0)
+    malfunction_report = models.OneToOneField(
+        MalfunctionReport, on_delete=models.SET_DEFAULT, default=0
+    )
+    user = models.OneToOneField(User, on_delete=models.SET_DEFAULT, default=0)
+    datetime = models.DateTimeField(auto_now_add=True)
+    description = models.TextField()
+    image = models.ImageField(upload_to="media/malfunction_reports")
