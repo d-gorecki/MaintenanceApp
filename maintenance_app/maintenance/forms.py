fc3a9ea0c4e1f847311d441bd054a67e5db2bce0
d-gorecki <ja.dawidgorecki@gmail.com>
2022-10-03 10:41:13 +0200

    refactor: forms frontend changed (bootstrap form-select)

--- maintenance_app/maintenance/forms.py
+++ maintenance_app/maintenance/forms.py
@@ -1,9 +1,20 @@
 from django import forms
 from .models import MaintenanceType
 from machines.models import MachineGroup
+from django.forms import Textarea, Select
 
 
 class MaintenanceTypeForm(forms.ModelForm):
     class Meta:
         model = MaintenanceType
-        fields = "__all__"
+        fields = ("type", "machine_group", "description")
+        widgets = {
+            "description": Textarea(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Malfunction description",
+                }
+            ),
+            "machine_group": Select(attrs={"class": "form-select"}),
+            "type": Select(attrs={"class": "form-select"}),
+        }
--- maintenance_app/malfunctions/forms.py
+++ maintenance_app/malfunctions/forms.py
@@ -4,7 +4,9 @@ from machines.models import Machine
 
 class ReportForm(forms.Form):
     machine = forms.ModelChoiceField(
-        queryset=Machine.objects.all(), help_text="Machine"
+        queryset=Machine.objects.all(),
+        help_text="Machine",
+        widget=forms.Select(attrs={"class": "form-select"}),
     )
     description = forms.CharField(
         widget=forms.Textarea(
