2125d1b1093400cd28074587e776c0c5df8c8c3b
d-gorecki <ja.dawidgorecki@gmail.com>
2022-10-09 23:33:39 +0200

    refactor: typing added

--- maintenance_app/dashboard/views.py
+++ maintenance_app/dashboard/views.py
@@ -1,4 +1,7 @@
+from typing import Union
+
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import QuerySet
 from django.shortcuts import render
 from plotly.offline import plot
 from plotly.graph_objs import Pie
@@ -7,25 +10,42 @@ from maintenance.models import MaintenanceReport
 from machines.models import Machine
 from django.views import View
 from maintenance_app.mixins import ManagerGroupTestMixin
+from django.http import HttpRequest, HttpResponse
 
 
 class Dashboard(LoginRequiredMixin, ManagerGroupTestMixin, View):
-    def get(self, request):
-        labels = ["available", "nonworking"]
-        values = [
+    """Dashboard class-based view. Access restricted only to users with manager group.
+    Consist of 4 sub-views(divs): 1. pie-chart (working/not working machines), 2. Three latest
+    malfunction reports. 3. Three latest maintenance reports. 4. Three latest service reports."""
+
+    def get(self, request: HttpRequest) -> HttpResponse:
+        labels: list[str] = ["available", "nonworking"]
+        values: list[int] = [
             Machine.objects.filter(machine_status="available").count(),
             Machine.objects.filter(machine_status="nonwork").count(),
         ]
-        plot_div = plot(
+        plot_div: str = plot(
             [Pie(labels=labels, values=values, textinfo="label+percent")],
             output_type="div",
         )
 
-        malfunctions_reports = MalfunctionReport.objects.all().order_by("-id")[:3]
-        maintenance_reports = MaintenanceReport.objects.all().order_by("-id")[:3]
-        malfunctions_services = ServiceReport.objects.all().order_by("-id")[:3]
+        malfunctions_reports: QuerySet[
+            MalfunctionReport
+        ] = MalfunctionReport.objects.all().order_by("-id")[:3]
+        maintenance_reports: QuerySet[
+            MaintenanceReport
+        ] = MaintenanceReport.objects.all().order_by("-id")[:3]
+        malfunctions_services: QuerySet[
+            ServiceReport
+        ] = ServiceReport.objects.all().order_by("-id")[:3]
 
-        context = {
+        context: dict[
+            str,
+            Union[
+                str,
+                QuerySet[Union[MalfunctionReport, MaintenanceReport, ServiceReport]],
+            ],
+        ] = {
             "plot_div": plot_div,
             "malfunctions_reports": malfunctions_reports,
             "maintenance_reports": maintenance_reports,
