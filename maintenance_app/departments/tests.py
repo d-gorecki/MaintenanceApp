45de8ce1ef16ced72bf81f187d1d0a618da96838
Dawid Górecki <ja.dawidgorecki@gmail.com>
2022-10-16 18:08:47 +0200

    feat: unit tests for machines.models added

--- maintenance_app/departments/tests.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from .models import Department
-
-
-@pytest.mark.django_db
-def test_department_create():
-    Department.objects.create(name="test_department")
-    assert Department.objects.count() == 1
---
+++ maintenance_app/machines/tests/test_models.py
@@ -0,0 +1,114 @@
+from django.test import TestCase
+from machines.models.machine import Machine
+from machines.models.machine_group import MachineGroup
+from datetime import datetime
+from departments.models import Department
+from django.utils.timezone import now as django_now
+
+
+class MachineGroupTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        MachineGroup.objects.create(name="Test-group")
+
+    def test_name_label(self):
+        name = MachineGroup.objects.get(id=1)
+        field_label = name._meta.get_field("name").verbose_name
+        self.assertEqual(field_label, "name")
+
+    def test_name_max_length(self):
+        name = MachineGroup.objects.get(id=1)
+        max_length = name._meta.get_field("name").max_length
+        self.assertEqual(max_length, 100)
+
+    def test_obj_str_is_name(self):
+        name = MachineGroup.objects.get(id=1)
+        expected_obj_str = f"{name.name}"
+        self.assertEqual(str(name), expected_obj_str)
+
+
+class MachineTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        MachineGroup.objects.create(name="Test-group")
+        Department.objects.create(name="Test-department")
+        Machine.objects.create(
+            factory_number="11111",
+            machine_group=MachineGroup.objects.get(id=1),
+            name="Test-name",
+            number="1111",
+            producer="test",
+            purchase_data=django_now(),
+            department=Department.objects.get(id=1),
+            machine_status="available",
+        )
+
+    def test_factory_number_label(self):
+        factory_number = Machine.objects.get(id=1)
+        field_label = factory_number._meta.get_field("factory_number").verbose_name
+        self.assertEqual(field_label, "factory number")
+
+    def test_name_max_length(self):
+        factory_number = Machine.objects.get(id=1)
+        max_length = factory_number._meta.get_field("factory_number").max_length
+        self.assertEqual(max_length, 100)
+
+    def test_machine_group_label(self):
+        machine_group = Machine.objects.get(id=1)
+        field_label = machine_group._meta.get_field("machine_group").verbose_name
+        self.assertEqual(field_label, "machine group")
+
+    def test_name_label(self):
+        name = Machine.objects.get(id=1)
+        field_label = name._meta.get_field("name").verbose_name
+        self.assertEqual(field_label, "name")
+
+    def test_name_max_length(self):
+        name = Machine.objects.get(id=1)
+        max_length = name._meta.get_field("name").max_length
+        self.assertEqual(max_length, 100)
+
+    def test_number_label(self):
+        number = Machine.objects.get(id=1)
+        field_label = number._meta.get_field("number").verbose_name
+        self.assertEqual(field_label, "number")
+
+    def test_number_max_length(self):
+        number = Machine.objects.get(id=1)
+        max_length = number._meta.get_field("number").max_length
+        self.assertEqual(max_length, 100)
+
+    def test_producer_label(self):
+        machine = Machine.objects.get(id=1)
+        field_label = machine._meta.get_field("producer").verbose_name
+        self.assertEqual(field_label, "producer")
+
+    def test_producer_max_length(self):
+        machine = Machine.objects.get(id=1)
+        max_length = machine._meta.get_field("producer").max_length
+        self.assertEqual(max_length, 100)
+
+    def test_purchase_data_label(self):
+        machine = Machine.objects.get(id=1)
+        field_label = machine._meta.get_field("purchase_data").verbose_name
+        self.assertEqual(field_label, "purchase data")
+
+    def test_department_label(self):
+        machine = Machine.objects.get(id=1)
+        field_label = machine._meta.get_field("department").verbose_name
+        self.assertEqual(field_label, "department")
+
+    def test_machine_status_label(self):
+        machine = Machine.objects.get(id=1)
+        field_label = machine._meta.get_field("machine_status").verbose_name
+        self.assertEqual(field_label, "machine status")
+
+    def test_machine_status_max_length(self):
+        machine = Machine.objects.get(id=1)
+        max_length = machine._meta.get_field("machine_status").max_length
+        self.assertEqual(max_length, 32)
+
+    def test_obj_str_is_name(self):
+        machine = Machine.objects.get(id=1)
+        expected_obj_str = f"{machine.factory_number}: {machine.name}"
+        self.assertEqual(str(machine), expected_obj_str)
